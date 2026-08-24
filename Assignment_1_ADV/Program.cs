74cf3b87ecd467bfcaea2aec4e22345e8976fd0b
marwanmohameddd2006-ai <marwanmohameddd2006@gmail.com>
2026-08-24 20:16:15 +0300

    Add project files.

---
+++ Assignment_1_ADV/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_1_ADV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
