2d236dd4986b5a063d6a2cbce2ffa59977f54034
marwanmohameddd2006-ai <marwanmohameddd2006@gmail.com>
2026-08-24 20:25:01 +0300

    Q01

--- Assignment_1_ADV/Program.cs
+++ Assignment_1_ADV/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            //Q:  What is a generic class? Why use generics?
+
+            /*Ans
+             * What ==> generic allow you to write class with type pramaters
+                        The actual type is specified when the cod is used
+                       
+             * Why ==> Typ Safe
+                       Reusable
+                       Better Performance
+                       Clean Code
+             */
+            #endregion
         }
     }
 }
